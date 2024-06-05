02778fe6e44865adac1b8edbf11945912c04d94d
Glen Perera <123937629+jumangl@users.noreply.github.com>
2024-06-06 00:37:59 +0530

    Added Pages

---
+++ register.php
@@ -0,0 +1,14 @@
+<?php
+include 'connect.php';
+
+if(isset($_POST['signUp'])){
+    $firstName = $_POST['fName'];
+    $lastName = $_POST['lName'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password = md5($password);
+
+}
+
+
+?>
